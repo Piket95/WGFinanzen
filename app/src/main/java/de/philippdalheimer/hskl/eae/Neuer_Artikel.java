7522f54c1ebe49ec11a115b61af7c1b573872094
Dennis Adam <dead0003@stud.hs-kl.de>
2019-05-28 19:05:24 +0200

    Angelegt: - Activity Details angelegt - Activity Neuer_Artikel angelegt - Fragment Feed (Liste) angelegt - Fragment Bewohner_einladen angelegt - Fragment WG_Beitreten angelegt - Fragment Ziele angelegt
    - alle dazugehörigen Layoutdateien generiert

---
+++ app/src/main/java/de/philippdalheimer/hskl/eae/Neuer_Artikel.java
@@ -0,0 +1,13 @@
+package de.philippdalheimer.hskl.eae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Neuer_Artikel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neuer__artikel);
+    }
+}
